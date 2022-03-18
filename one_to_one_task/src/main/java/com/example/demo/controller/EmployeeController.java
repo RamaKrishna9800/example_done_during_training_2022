d01f8e2a82450f70ce6191d28ea4e6e4942db0ee
Gangadhara.G <Gangadhara.G@EXIND3010-120.allfon.in>
2022-03-18 18:00:39 +0530

    change

--- one_to_one_task/src/main/java/com/example/demo/controller/EmployeeController.java
+++ one_to_one_task/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 @RequestMapping(path = "/api/v1")
+@CrossOrigin(origins="*")
 
 public class EmployeeController {
 	private EmployeeService service;
